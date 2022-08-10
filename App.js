8e0f79fa867e942ae53f0e0e347ab77ad0bd445e
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-08-10 14:35:50 +0700

    Adding Context

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import IndexScreen from './src/screens/IndexScreen';
 import {NavigationContainer} from '@react-navigation/native';
+import {BlogProvider} from './src/context/BlogContext';
 
 const Stack = createNativeStackNavigator();
 
@@ -19,4 +20,10 @@ const App = () => {
   );
 };
 
-export default App;
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
