7f00e74663a69be7e6ecd51809d4969104d607d0
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-08-11 06:13:27 +0700

    Retrieving SIngle Posts

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {Context} from '../context/BlogContext';
 
-const ShowScreen = () => {
+const ShowScreen = ({route}) => {
+  const {state} = useContext(Context);
+  const {id} = route.params;
+  const blogPost = state.find(blogPost => blogPost.id === id);
+
   return (
     <View>
-      <Text>Show Screen</Text>
+      <Text>{blogPost.title}</Text>
     </View>
   );
 };
